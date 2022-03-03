53a34ae35fe8ae95c8c5ca9c26b0bd5ce4a83ff7
Carlos Daniel Cabral <86313878+carlosdanielcabral@users.noreply.github.com>
2022-03-03 18:14:00 -0300

    Update index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,13 +2,9 @@ import { combineReducers } from 'redux';
 import user from './user';
 import wallet from './wallet';
 
-// Configure os seus reducers.
-
 const reducer = combineReducers({
   user,
   wallet,
 });
-
-// ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
 
 export default reducer;
